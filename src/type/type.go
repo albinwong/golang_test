d26a42769c785ff394f6d653c11cc324ef787737
Albin Wong <albinwong@sina.com>
2019-01-01 16:02:55 +0800

    go type system

--- src/type/type.go
+++ src/type/type.go
@@ -1,9 +1,41 @@
+// type system 类型系统
 package main
 
 import (
 	"fmt"
 )
+type Integer int
+
+// func (a Integer) Less(b Integer) bool {
+// 	res := a < b
+// 	return res
+// }
+
+// func main() {
+// 	var a Integer = 1
+// 	if a.Less(2) {
+// 		fmt.Println(a, "Less 2")
+// 	}
+
+
+/*func (a *Integer) Add(b Integer) {
+	*a += b
+	return
+}*/
+
+func (a Integer) Add(b Integer) {
+	a += b
+	return
+}
 
 func main() {
-	fmt.Println("a")
+	var a Integer = 3
+	a.Add(2)
+		fmt.Println("a = ",a)
 }
+
+
+/*func main() {
+	var v1 interface{} = 1
+	fmt.Println(v1)
+}*/
